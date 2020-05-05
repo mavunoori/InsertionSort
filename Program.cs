5810ac190229d43792daaf4869458985b50333fe
avunoma <33365065+avunoma@users.noreply.github.com>
2020-05-05 08:27:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 3, 22, 43, 23, 2, 0, 2 };
+            int l = 1;
+            int high = arr.Length;
+
+            for (int i = l; i < high; i++)
+            {
+                int key = arr[i];
+                int j = i-1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            
+            for (int i = 0; i < high; i++)
+                Console.Write(arr[i] + " ");
+        }
+    }
+}
